54c7de65f4fa307eb6caac5ec43f93bf8b5e8696
Tony Tsai <tonyny58@gmail.com>
2020-08-04 19:17:00 +0800

    20200804

--- 中位數以及排名.sql
+++
@@ -1,5 +0,0 @@
-select distinct c.sale from (select a1.name,a1.sale,count(a2.name) as sale_rank
-from sale a1,sale a2
-where a1.sale<a2.sale or (a1.sale=a2.sale and a1.name=a2.name)
-group by a1.sale,a1.name ) as c 
-where c.sale_rank between (select count(*) from sale)/2-0.5 and (select count(*) from sale)/2+0.5
--- 初始化3.sql
+++
@@ -1,3 +0,0 @@
-select * from permission_table 
-group by permission 
-having count(name)=1
